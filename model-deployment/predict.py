b238e33a418b478d667fe2b7b3bf04d55e28a02e
Oghenefejiro Ephraim Esosuota <ephraim2039@gmail.com>
2022-07-04 14:34:44 +0100

    Added Docker file, project complete

--- model-deployment/predict.py
+++ model-deployment/predict.py
@@ -1,5 +1,9 @@
 import joblib
 
+from flask import Flask, request, jsonify
+
+
+app = Flask('duration-prediction')
 
 with open('web-service/model.bin', 'rb') as f_in:
     dv, model = joblib.load(f_in)
@@ -16,3 +20,22 @@ def predict(features):
     X = dv.transform(features)
     preds = model.predict(X)
     return preds[0]
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+
+    features = prepare_features(ride)
+    pred = predict(features)
+
+    result = {
+        'duration' : pred
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
